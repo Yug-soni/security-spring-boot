3a9c04e379d1de2736b3520838422d723e2692f7
Yug-soni <yugsoni8946842393@gmail.com>
2021-07-25 17:26:10 +0530

    package renamed

--- src/main/java/com/yug/app/Application.java
+++ src/main/java/com/yug/app/Application.java
@@ -1,4 +1,4 @@
-package com.yug.security;
+package com.yug.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/yug/app/ApplicationTests.java
+++ src/test/java/com/yug/app/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.yug.security;
+package com.yug.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
