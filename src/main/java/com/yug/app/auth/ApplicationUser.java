aa5cf9dcd768d858b7f0d056b8bcb6db21c42fd8
Yug-soni <yugsoni8946842393@gmail.com>
2021-07-27 12:50:37 +0530

    added the custom class which is used for making the application users.

---
+++ src/main/java/com/yug/app/auth/ApplicationUser.java
@@ -0,0 +1,55 @@
+package com.yug.app.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@AllArgsConstructor
+public class ApplicationUser implements UserDetails {
+
+    private final Collection<? extends GrantedAuthority> grantedAuthorities;
+    private  final String password;
+    private  final String username;
+    private  final boolean isAccountNonExpired;
+    private  final boolean isAccountNonLocked;
+    private  final boolean isCredentialsNonExpired;
+    private  final boolean isEnabled;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.grantedAuthorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.isAccountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.isAccountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.isCredentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.isEnabled;
+    }
+}
